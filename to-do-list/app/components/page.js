1f2b678986341d8f8ee53bd904fa36bb8c8427f2
Arda Pehlevan <arda.pehlevan98@gmail.com>
2024-04-21 17:52:15 +0200

    All required functionalities working

--- to-do-list/app/components/page.js
+++ to-do-list/app/components/page.js
@@ -1,13 +1,34 @@
 "use client";
-import InputModal from "./InputModal";
+
 import styles from "../page.module.css"
-import { FloatButton, Table } from 'antd';
+import { FloatButton, Table, Radio, Modal, Button, Input } from 'antd';
 import { FileTextOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 
 
 
+
 export default function Page() {
+    const [modalVisible, setModalVisible] = useState(false); // Initialize visible state to false
+    const [inputTask, setInputTask] = useState('');
+    const [inputName, setInputName] = useState('');
+    const [prio, setPrio] = useState('Low');
+
+
+    const options = [
+        {
+            label: 'Low',
+            value: 'Low',
+        },
+        {
+            label: 'Medium',
+            value: 'Medium',
+        },
+        {
+            label: 'High',
+            value: 'High',
+        },
+    ];
 
     const columns = [
         {
@@ -35,8 +56,6 @@ export default function Page() {
                 },
 
             ],
-
-            //sorter: (a, b) => a.age - b.age,
             onFilter: (value, record) => record.priority.indexOf(value) === 0,
         },
         {
@@ -45,35 +64,40 @@ export default function Page() {
 
         },
         {
-            title: 'Status',
-            dataIndex: 'status',
+            title: 'Date added',
+            dataIndex: 'time',
+            sorter: (a,b) => new Date(a.time) - new Date(b.time),
 
         },
     ];
     const [data, setData] = useState([
-        { key: '1', task: 'ananin amina koymaca', priority: 'High', assigned: '123 Elm Street', status: '123 Elm Street' },
-        { key: '2', task: 'listen berfin yappin', priority: 'low', assigned: '123 Elm Street', status: '123 Elm Street' },
-        { key: '3', task: '78 dk tuvaltte sic', priority: 'medium', assigned: '123 Elm Street', status: '123 Elm Street' },
-        { key: '4', task: 'bulasik yika', priority: 'medium', assigned: '123 Elm Street', status: '123 Elm Street' },
+        { key: '1', task: 'Write Thesis', priority: 'High', assigned: 'Max Mustermann', time: new Date(86400000).toLocaleString()},
+        { key: '2', task: 'Clean the dishes', priority: 'Low', assigned: 'Jolene', time: new Date(864078000).toLocaleString()},
+        { key: '3', task: 'Go for a run', priority: 'Medium', assigned: 'Florian', time: new Date(86478345000).toLocaleString()},
+        { key: '4', task: 'Do nothing for 1 hour', priority: 'High', assigned: 'Christian', time: new Date(37485943685).toLocaleString()},
         // Other initial rows
     ]);
 
-    const onChange = (pagination, filters, sorter, extra) => {
-        console.log('params', pagination, filters, sorter, extra);
-      };
+    const addRow = () => {
 
-    const [modalVisible, setModalVisible] = useState(false); // Initialize visible state to false
-    const [inputText, setInputText] = useState('');
-
-    const addRow = (task) => {
-
-        const newRow = { key: String(data.length + 1), task: task, priority: 'low', assigned: "name", status: 'in progres' };
+        const newRow = { key: String(data.length + 1), task: inputTask, priority: prio, assigned: inputName, time: currentDate()};
         setData([...data, newRow]);
-      };
+    };
 
+
+    const handlePrioChange = ({ target: { value } }) =>{
+        setPrio(value)
+    }
+    const handleNameChange = (e) => {
+        setInputName(e.target.value);
+    };
+    
+    const handleInputChange = (e) => {
+        setInputTask(e.target.value);
+    };
     const handleModalOk = (text) => {
-        setInputText(text);
-        addRow(text);
+        addRow();
+        handleInputChange(text);
         setModalVisible(false);
     };
 
@@ -81,35 +105,65 @@ export default function Page() {
         setModalVisible(false);
     };
 
+    
+
+    const currentDate = () => {
+        return new Date().toLocaleString()
+    }
+
     return (
         <html lang="en">
 
             <body >
                 <Table columns={columns}
                     dataSource={data}
-                    onChange={onChange}
                     pagination={false}
                     showSorterTooltip={{
                         target: 'sorter-icon',
                     }} />
-                <br/>
+                <br />
                 <div>
-                <InputModal 
+                    <Modal
+                        title="Enter Task"
                         visible={modalVisible}
-                        onCancel={handleModalCancel}
-                        onOk={handleModalOk}
+                        footer={[
+                            <Button key="cancel" onClick={handleModalCancel}>
+                                Cancel
+                            </Button>,
+                            <Button key="ok" type="primary" onClick={handleModalOk}>
+                                OK
+                            </Button>,
+                        ]}>
+                        <b>Task:</b><Input
+                            type="text"
+                            placeholder='Enter your task'
+                            value={inputTask}
+                            onChange={handleInputChange}
+                        />
+                        <br /><br />
+                        <b>Select priority:</b>
+                        <br/>
+                        <Radio.Group options={options} onChange={handlePrioChange} value={prio} optionType="button"/>
+                        <br />
+                        <br />
+                        <b>Assigned to:</b><Input 
+                        type="text"
+                        placeholder='Enter who to assign the task to' 
+                        value={inputName}
+                        onChange={handleNameChange}/>
+                        <br />
+                    </Modal>
+                    </div>
+                    <FloatButton
+                        icon={<FileTextOutlined />}
+                        onClick={() => setModalVisible(true)}
+                        description="New Task"
+                        type='primary'
+                        shape="square"
+                        style={{
+                            right: 24,
+                        }}
                     />
-                </div>
-                <FloatButton
-                    icon={<FileTextOutlined />}
-                    onClick={() => setModalVisible(true)}
-                    description="New Task"
-                    type='primary'
-                    shape="square"
-                    style={{
-                        right: 24,
-                    }}
-                />
             </body>
         </html>
     );
